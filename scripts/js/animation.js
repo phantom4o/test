235ce23d50169dcfc2a17509eb6270863edeb3a7
Victor <viktordm89@gmail.com>
2024-04-21 16:30:47 +0300

    aside skelly finished

--- scripts/js/animation.js
+++ scripts/js/animation.js
@@ -3,16 +3,41 @@
 function toggleMenu() {
     var barsIcon = document.getElementById('barsIcon');
     var closeIcon = document.getElementById('closeIcon');
-    var myNav = document.getElementById('side-menu');
+    var myNav = document.querySelector('.side-menu');
 
-    if (barsIcon.style.display === 'none') {
+    if (barsIcon.style.display === 'none' || barsIcon.style.display === '') {
         barsIcon.style.display = 'inline-block';
         closeIcon.style.display = 'none';
-        myNav.style.right = '-780px'; 
+        myNav.style.right = '-1000px';
     } else {
         barsIcon.style.display = 'none';
         closeIcon.style.display = 'inline-block';
         myNav.style.right = '0';
-        
+
+        // Hiding dropdown sections when clicking bars
+        var abtSection = document.querySelector('.abt_small_secntion');
+        var cerfSection = document.querySelector('.cerf_small_secntion');
+        abtSection.style.display = 'none';
+        cerfSection.style.display = 'none';
     }
 }
+
+// Aside bar Section drop down
+document.getElementById('element2').addEventListener('click', function () {
+    var abtSection = document.querySelector('.abt_small_secntion');
+    if (abtSection.style.display === 'none' || abtSection.style.display === '') {
+        abtSection.style.display = 'block';
+    } else {
+        abtSection.style.display = 'none';
+    }
+});
+
+document.getElementById('element3').addEventListener('click', function () {
+    var cerfSection = document.querySelector('.cerf_small_secntion');
+    if (cerfSection.style.display === 'none' || cerfSection.style.display === '') {
+        cerfSection.style.display = 'block';
+    } else {
+        cerfSection.style.display = 'none';
+    }
+});
+
