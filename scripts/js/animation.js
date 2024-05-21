05bb764e485d81de1fe7bc575a7df660973ea25d
Victor <viktordm89@gmail.com>
2024-05-21 21:02:42 +0300

    contact pc finished

--- scripts/js/animation.js
+++ scripts/js/animation.js
@@ -69,5 +69,5 @@ window.addEventListener('load', function() {
     setTimeout(function() {
       loader.style.display = 'none';
       document.body.style.overflowY = 'auto';
-    }, 900);
+    }, 700);
   });
